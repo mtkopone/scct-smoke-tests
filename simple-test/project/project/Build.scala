cbd6ae53e077f54ea36c87be89188d76c1795aeb
Mikko Koponen <mikko.koponen@reaktor.fi>
2012-06-10 14:39:14 +0300

    simple-test project now works with both maven 3 and sbt 0.13 w/ scala 2.9.2.

---
+++ simple-test/project/project/Build.scala
@@ -0,0 +1,7 @@
+import sbt._
+
+object PluginDef extends Build {
+  override lazy val projects = Seq(root)
+  lazy val root = Project("plugins", file(".")) dependsOn(scctPlugin)
+  lazy val scctPlugin = uri("file:///Users/mtkopone/projects/scct/sbt-scct")
+}
