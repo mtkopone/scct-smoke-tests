9b3cbdda01fd493e650534385e866184d84300a8
Mikko Koponen <mikko.koponen@reaktor.fi>
2012-08-02 14:04:55 +0300

    Script to wipe local env.

---
+++ clean-repos.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+rm -rf ~/.m2/repository/reaktor/scct_*
+rm -rf ~/.ivy2/local/reaktor*
+rm -rf ~/.ivy2/cache/scala_2.9.1/sbt_0.11.2/reaktor*
+rm -rf ~/.ivy2/cache/scala_2.9.1/sbt_0.12/reaktor*
+rm -rf ~/.ivy2/cache/scala_2.9.2/sbt_0.13/reaktor*
+rm -rf ~/.ivy2/cache/reaktor*
+rm -rf ~/.ivy2/cache/resolved-reaktor*
